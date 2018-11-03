12915f090a0d1f51ce78699495d9ee150762390c
nakata yuki <nasutaro211@gmail.com>
2018-11-03 14:41:42 +0900

    first commit

---
+++ lochat/lochat/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  lochat
+//
+//  Created by 中田　優樹 on 2018/11/03.
+//  Copyright © 2018年 nakatayuki. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
