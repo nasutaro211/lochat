d76999b36c22b6ff2c77d000e98e01c478b7b3ac
jojimanabe <manajyo511@gmail.com>
2018-11-03 16:44:24 +0900

    complete preparetabview of mainstoryboard

---
+++ lochat/lochat/HighlightsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HighlightsTableViewCell.swift
+//  lochat
+//
+//  Created by manabejoji on 2018/11/03.
+//  Copyright © 2018 nakatayuki. All rights reserved.
+//
+
+import UIKit
+
+class HighlightsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
