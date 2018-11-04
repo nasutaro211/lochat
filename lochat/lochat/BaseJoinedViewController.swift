924cf903a0df07d07bcbec15fbbe3f5308d0a6dd
nakata yuki <nasutaro211@gmail.com>
2018-11-04 13:59:29 +0900

    enable to get frames

---
+++ lochat/lochat/BaseJoinedViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BaseJoinedViewController.swift
+//  lochat
+//
+//  Created by 中田　優樹 on 2018/11/04.
+//  Copyright © 2018年 nakatayuki. All rights reserved.
+//
+
+import UIKit
+
+class BaseJoinedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
